6a0551e18b5433d43e572975658f683e8494ae77
spgowrisankar <spgowrisankar@gmail.com>
2020-07-14 17:52:36 +0530

    About Page added

--- js/main.js
+++ js/main.js
@@ -12,5 +12,10 @@ $(document).ready(function(){
     $('about').on('click', function(){
     $(this).closest.removeClass('header').closest.addClass('transparent-header');
     });
+    $('about').on('click', function(){
+        $('about').on('click', function(){
+        $(this).closest.removeClass('menu-link').closest.addClass('transparent-link');
+        });
+    });
 
 });
